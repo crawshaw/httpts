552e67403519ed1b5b91bc7f4802894fe020d4db
David Crawshaw <david@zentus.com>
2024-09-16 10:36:51 -0700

    cmd/tailweb: add docs

--- cmd/tailweb/tailweb.go
+++ cmd/tailweb/tailweb.go
@@ -1,10 +1,21 @@
 // The tailweb command serves out the current directory to your tailnet.
+//
+// This is the tailscale equivalent of the classic python one-liner:
+//
+//	python -m http.server 8000
+//
+// Install tailweb with:
+//
+//	go install github.com/crawshaw/httpts/cmd/tailweb@latest
+//
+// then to serve the current directory, run:
+//
+//	tailweb
 package main
 
 import (
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 
@@ -21,16 +32,14 @@ func main() {
 	flag.Usage = usage
 	hostname, err := os.Hostname()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	tsHostname := flag.String("hostname", hostname+"-tailweb", "hostname to use for the server")
 	flag.Parse()
-	log.SetPrefix("tailweb: ")
-	log.SetFlags(0)
 
 	dir, err := os.Getwd()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	if args := flag.Args(); len(args) == 1 {
 		dir = flag.Args()[0]
@@ -39,5 +48,5 @@ func main() {
 	}
 
 	s := httpts.Server{Handler: http.FileServer(http.Dir(dir))}
-	log.Fatal(s.Serve(*tsHostname))
+	panic(s.Serve(*tsHostname))
 }
